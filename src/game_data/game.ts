52863862fe4ae18ce66ae98fe166a081ec9db4ac
Frank Fenton <echelon.iiii@gmail.com>
2020-01-08 14:38:33 +1100

    ♻ rework how players are rehydrated

--- src/game_data/game.ts
+++ src/game_data/game.ts
@@ -1,7 +1,7 @@
 import { Player } from './player';
 import { Plot } from './plot';
 import { Relationship } from './relationship';
-import { Type } from 'class-transformer';
+import { Type, Exclude } from 'class-transformer';
 
 export class Game {
   name: string;
@@ -10,6 +10,9 @@ export class Game {
   @Type(() => Plot)
   plots: Plot[] = [];
   seed: string;
+
+  @Exclude()
+  allRelationships: Map<string, Relationship> = new Map();
 
   constructor(name: string) {
     this.name = name;
@@ -22,23 +25,8 @@ export class Game {
     for (const plot of this.plots) {
       plot.rehydrate(this);
     }
-    // this.players.forEach((p) => p.relationships = []); // class transformers seems to never run the constructor, so this needs to be inited
-    const allRelationships: Set<string> = new Set();
     for (const player of this.players) {
-      for (const otherPerson of player.otherPeople) {
-        const otherPlayer = this.getPlayer(otherPerson.playerTitle);
-        const relationship = new Relationship(player, otherPlayer, otherPerson.strength, otherPerson.alignment);
-        if (!allRelationships.has(relationship.uniqueString)) {
-          const otherPeopleDetailsAboutMe = otherPlayer.otherPeople.find((p) => p.playerTitle === player.title);
-          if (otherPeopleDetailsAboutMe == null || otherPeopleDetailsAboutMe.alignment !== relationship.alignment || otherPeopleDetailsAboutMe.strength !== relationship.strength) {
-            console.warn(`The relationship from ${player.title} to ${otherPlayer.title} does not seem to match. this could screw some stuff up`);
-          }
-          allRelationships.add(relationship.uniqueString);
-          player.relationships.push(relationship);
-          otherPlayer.relationships.push(relationship);
-        }
-      }
-      player.otherPeople = []; // relationships set, go back to calculated
+      player.rehydrate(this);
     }
   }
 }
--- src/game_data/player.ts
+++ src/game_data/player.ts
@@ -2,6 +2,7 @@ import { Power } from './power';
 import { Goal } from './goal';
 import { Expose, Exclude, Type } from 'class-transformer';
 import { Relationship, OtherPerson } from './relationship';
+import { Game } from './game';
 
 export class Player {
   firstName: string;
@@ -29,7 +30,7 @@ export class Player {
   @Type(() => OtherPerson)
   @Expose()
   get otherPeople(): OtherPerson[] {
-    if (this._otherPeople.length !== 0) {
+    if (this._otherPeople.length !== 0 || this.relationships.length === 0) {
       return this._otherPeople;
     }
     return this.relationships.map((rel) => OtherPerson.makeFromMe(this, rel));
@@ -62,5 +63,24 @@ export class Player {
 
   get playerSummary(): string {
     return `${this.fullName}(${this.title}) - ${this.otherPeopleSummary} - ${this.fullPowers} - ${this.fullGoals}`;
+  }
+
+  rehydrate(game: Game): void {
+    for (const otherPerson of this._otherPeople) {
+      const otherPlayer = game.getPlayer(otherPerson.playerTitle);
+      let relationship = new Relationship(this, otherPlayer, otherPerson.strength, otherPerson.alignment);
+      const relationshipHash = relationship.uniqueString
+      if (game.allRelationships.has(relationshipHash)) {
+        relationship = game.allRelationships.get(relationshipHash);
+      } else {
+        game.allRelationships.set(relationshipHash, relationship);
+        const otherPeopleDetailsAboutMe = otherPlayer.otherPeople.find((p) => p.playerTitle === this.title);
+        if (otherPeopleDetailsAboutMe == null || otherPeopleDetailsAboutMe.alignment !== relationship.alignment || otherPeopleDetailsAboutMe.strength !== relationship.strength) {
+          console.warn(`The relationship from ${this.title} to ${otherPlayer.title} does not seem to match. this could screw some stuff up`);
+        }
+      }
+      this.relationships.push(relationship);
+    }
+    this._otherPeople = []; // relationships set, go back to calculated
   }
 }
